16e2da4b3468657989d0f84bcd29b3d65e6c5fbc
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2018-02-20 08:39:28 +0900

    Flat scope

---
+++ flat_scope.rb
@@ -0,0 +1,21 @@
+# 入れ子構造のレキシカルスコープ = フラットスコープ
+
+my_var = "成功"
+
+class MyClass
+  # スコープゲートによりmy_varは呼び出せない
+  def my_method
+  # スコープゲートによりmy_varは呼び出せない
+  end
+end
+
+YourClass = Class.new do
+  # my_varを表示できる
+  puts "クラス定義の中では#{my_var}"
+
+  define_method :my_method do
+    # Module#define_methodの中なら呼び出せる
+    puts "メソッド定義の中でも#{my_var}"
+  end
+
+end
